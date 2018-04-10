63d013a21ee3bc2b72da22d24a734f615e43fb6a
DIR\o.alvarado.quiroga <o.alvarado.quiroga@accenture.com>
2018-04-10 11:23:56 -0500

    app pipes terminada. curso angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  nombre = "Oscar";
+  nombre2 = "oscar edUardO alvaRAdo quiRoga"
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a:number = 0.234;
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primerra",
+      casa: "19"
+    }
+  }
+
+  valorDePromesa = new Promise( (resolve, reject) => {
+    setTimeout(()=> resolve('Llego la data'), 3500);
+  });
+
+  fecha = new Date();
+
+  video = "3F0XDQy9wE8";
+
+  activar:boolean = true;
 }
